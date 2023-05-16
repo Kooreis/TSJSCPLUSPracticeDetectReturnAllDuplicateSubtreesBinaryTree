bfc0e1ef1b9359606b74509b9c30201e1d09cbc0
Kooreis <kooreis.official@gmail.com>
2023-05-16 14:07:59 -0500

    This update introduces a Node class that will be used to create nodes for the binary tree. Each node has a data property and left and right properties for its child nodes.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,3 @@
-Here is a JavaScript solution for the problem:
-
-```javascript
 class Node {
     constructor(data) {
         this.data = data;
@@ -8,35 +5,3 @@ class Node {
         this.right = null;
     }
 }
-
-function findDuplicateSubtrees(root) {
-    let count = new Map();
-    let ans = [];
-    collect(root, count, ans);
-    return ans;
-}
-
-function collect(node, count, ans) {
-    if (node == null) return '#';
-    let serial = node.data + ',' + collect(node.left, count, ans) + ',' + collect(node.right, count, ans);
-    count.set(serial, (count.get(serial) || 0) + 1);
-    if (count.get(serial) === 2)
-        ans.push(node);
-    return serial;
-}
-
-let root = new Node(1);
-root.left = new Node(2);
-root.right = new Node(3);
-root.left.left = new Node(4);
-root.right.left = new Node(2);
-root.right.right = new Node(4);
-root.right.left.left = new Node(4);
-
-let duplicateSubtrees = findDuplicateSubtrees(root);
-for(let i = 0; i < duplicateSubtrees.length; i++) {
-    console.log(duplicateSubtrees[i].data);
-}
-```
-
-This solution uses a depth-first search to serialize the tree. It uses a map to keep track of all subtrees that have been seen before and their counts. If a serialized subtree is seen more than once, it is added to the answer array. The serialized subtree is returned to the parent call, which then uses it to serialize the larger subtree.
