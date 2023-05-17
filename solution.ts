1411d4ad0402c76597963e5c96869df9f8ba7a95
Kooreis <kooreis.official@gmail.com>
2023-05-17 14:07:59 -0500

    This update introduces the BinaryTree class, which will be used to create a binary tree. It has a root property to reference the root node, and methods to insert new nodes into the tree.

--- solution.ts
+++ solution.ts
@@ -1,11 +1,33 @@
-class Node {
-    data: number;
-    left: Node | null;
-    right: Node | null;
+class BinaryTree {
+    root: Node | null;
 
-    constructor(data: number) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
+    constructor() {
+        this.root = null;
+    }
+
+    insert(data: number) {
+        let newNode = new Node(data);
+
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node: Node, newNode: Node) {
+        if (newNode.data < node.data) {
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
     }
 }
